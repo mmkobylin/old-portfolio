db61ea245105cc1d93384cac7b835757177cecd1
mmkobylin <mmkobylin@gmail.com>
2020-12-02 09:01:16 +0000

    removing unused parts of jquery and links in index

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,26 +1,6 @@
 ((d) => {
 
     //getting items via Id
-    let show = d.getElementById("show");
-    let hideLess = d.getElementById("hide-less")
-    let hideMore = d.getElementById("hide-more")
 
-    let cardDisplay = d.getElementById("card");
-
-    // this is our state
-    // keep track of whether the items are showing
-    let invisible = true;
-
-    // every time the user clicks
-    show.addEventListener("click", () => {
-        // if display:none then set to inline, otherwise set to none
-        hideLess.style.display = invisible ? "inline" : "none";
-        hideMore.style.display = invisible ? "none" : "inline";
-
-        cardDisplay.style.display = invisible ? "inline" : "none";
-
-        // switch the value of invisible
-        invisible = !invisible;
-    });
 })(document);
 
